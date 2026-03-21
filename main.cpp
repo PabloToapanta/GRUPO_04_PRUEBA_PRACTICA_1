a43a6b776a9ab54ca03016bbdfaebc9cdcb642e1
Pablo Toapanta <205024625+PabloToapanta@users.noreply.github.com>
2026-03-20 22:31:18 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,25 @@
 #include <iostream>
 #include "include/ListaSecuencial.h"
-
+#include "include/ListaDobleEnlazada.h"
 int main() {
-    std::cout << "--- INICIANDO PRUEBAS DEL DESARROLLADOR 1 ---\n\n";
+    /*Juego miPartida;
+    miPartida.iniciarMenu(); // Llama al menú principal [cite: 63]
+    return 0;*/
+    ListaDobleEnlazada misEnemigos;
 
-    ListaSecuencial misTorres;
+    Enemigo e1 = {1, " Orco ", 100, 2, 0, 10}; //enemigo de prueba
+    
+    std::cout << "Insertando enemigo..." << std::endl; //hacemos uso de la funcion insertar enemigo
+    misEnemigos.insertarFinal(e1);
 
-    // 1. Creamos unas torres de prueba
-    Torre t1 = {1, "Arquero", "Basica", 3, 20, 2, 100};
-    Torre t2 = {2, "Canion", "Pesada", 8, 35, 3, 200};
-    Torre t3 = {3, "Mago", "Magica", 15, 50, 4, 300};
+    misEnemigos.recorrerAdelante();//probamos las funciones de mostrar y mover
+    std::cout << "Avanzando posiciones..." << std::endl;
+    misEnemigos.actualizarPosiciones();
+    misEnemigos.recorrerAdelante();
 
-    // 2. Probamos la inserción
-    std::cout << "Insertando torres...\n";
-    misTorres.insertarTorre(t1);
-    misTorres.insertarTorre(t2);
-    misTorres.insertarTorre(t3);
+    std::cout << "Eliminando enemigo..." << std::endl; //eliminamos enemigo
+    misEnemigos.eliminarDestruido(1);
+    misEnemigos.recorrerAdelante();
 
-    // 3. Probamos la validación de seguridad (intentar meter un ID que ya existe)
-    std::cout << "\nIntentando meter una torre con ID repetido (deberia dar error)...\n";
-    misTorres.insertarTorre(t2);
-
-    // 4. Mostramos cómo quedó el arreglo
-    misTorres.mostrarTorres();
-
-    // 5. Probamos eliminar una torre del medio (El Cañón, ID 2)
-    std::cout << "\nEliminando la torre con ID 2...\n";
-    misTorres.eliminarTorre(2);
-
-    // 6. Volvemos a mostrar para confirmar que el Mago tapó el hueco del Cañón
-    misTorres.mostrarTorres();
-
-    std::cout << "\n--- FIN DE LAS PRUEBAS. SI VES ESTO SIN ERRORES, TU MODULO FUNCIONA ---\n";
     return 0;
 }
